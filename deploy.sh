d41ecb91fdfdddf91ad819e446295b81d7295230
saba-pm <86867724+saba-pm@users.noreply.github.com>
2022-10-11 19:22:47 +0330

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,15 @@
 first=$(cat env.txt | gawk -F'=' '{print $1}')
 second=($(cat env.txt | gawk -F'=' '{print $2}'))
 COUNTER=0
+
 echo -e "{\n    \"family\":\"\",\n    \"containerDefinitions\":[\n        {\n            \"name:\"\"\",\n            \"image\":\"\",\n            \"environment\":[\n" >> config.json
 for i in ${first[@]} ; do
         echo -e "                {\n                   \"name\":\"$i\"\n                   \"value\":\"${second[$COUNTER]}\"\n                }," >> config.json
         COUNTER=$COUNTER+1
 done
 echo -e "\n            ],\n        }\n   ],\n}" >> config.json
+
+
+echo '---------------------------------------------'
+echo -e "Your config file has been created.\nRead the file contents:\n\ncat ./config.json"
+echo '---------------------------------------------'
